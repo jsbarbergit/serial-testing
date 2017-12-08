10daaab4340370d12b8dcd25401b9f27020ca12f
John Barber <jsbarber@gmail.com>
2017-12-08 16:09:25 +0000

    test scripts

---
+++ hello_com1.py
@@ -0,0 +1,21 @@
+import serial
+import hashlib
+from time import sleep
+
+port = 'COM1'
+ser = serial.Serial(port, 9600)
+start_frame = '<'
+end_frame = '>'
+count =0
+
+while True:
+	data = 'This is test message no: ' + str(count)
+	#Create md5 check sum
+	chksum = hashlib.md5(data).hexdigest()
+	#Build message frame including some extraneous data and 
+	message = "ignore_me" + start_frame + data +  chksum + end_frame + "and me"
+	x = ser.write(message)
+	count += 1
+	print "Sending message no: " + str(count)
+	sleep(0.1)
+ser.close()
